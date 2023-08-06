889f7b8f443db57c7d84e360760f3156c6a8fb49
Alex <47835666+Alco4998@users.noreply.github.com>
2023-08-06 16:56:02 +0800

    Initial Commit

---
+++ FuelFiller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FuelFiller
+//
+//  Created by Alexander Crew on 6/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FuelFiller/FuelFillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelFillerApp.swift
+//  FuelFiller
+//
+//  Created by Alexander Crew on 6/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct FuelFillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

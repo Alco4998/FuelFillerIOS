14e36fc396a0fadb51dd0b95bccb28c11dc9879f
Alex <47835666+Alco4998@users.noreply.github.com>
2023-08-10 11:15:49 +0800

    Added more views

---
+++ FuelFiller/Views/InsertView.swift
@@ -0,0 +1,26 @@
+//
+//  InsertView.swift
+//  FuelFiller
+//
+//  Created by Alexander Crew on 6/8/2023.
+//
+
+import SwiftUI
+
+struct InsertView: View {
+    var body: some View {
+        VStack {
+            List{
+                Text("yes")
+                Text("yes")
+                Text("yes")
+            }
+        }.foregroundColor(.gray)
+    }
+}
+
+struct InsertView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsertView()
+    }
+}
---
+++ FuelFiller/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  FuelFiller
+//
+//  Created by Alexander Crew on 6/8/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("I am a Settings page")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
